825bb2a1d672b2a764c6e3431cb3ddce5596c16e
ImAshuaige <605403793@qq.com>
2023-08-06 12:04:56 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import logo from './logo.svg';
-<<<<<<< HEAD
-import './App.css'; 
-=======
 import './App.css';
 //adding comment - mehak
 
->>>>>>> 3fab7bb884a3a2b448a081b9d03a43331fddcd2c
 function App() {
   return (
     <div className="App">
