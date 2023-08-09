21183ce21d8aaa1a942418780a0c89c8f8ed9476
ImAshuaige <605403793@qq.com>
2023-08-09 19:08:53 +0800

    search a certain date

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -10,61 +10,76 @@ const NasaPhoto = () => {
     const [selectedDate, setSelectedDate] = useState(null);
 
     useEffect(() => {
-        fetchPhoto();
-
-        async function fetchPhoto() {
-            const res = await fetch(
-                `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
-            );
-
-            const data = await res.json();
-            setPhotoData(data);
-            console.log(data);
+        if (selectedDate) {
+            fetchPhoto(selectedDate);
+        } else {
+            fetchDefaultPhoto();
         }
-    }, []);
+    }, [selectedDate]);
 
-    if (!photoData) return <div />;
+    const fetchPhoto = async (date) => {
+        const formattedDate = date.toISOString().split("T")[0];
 
+        const res = await fetch(
+            `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${formattedDate}`
+        );
+
+        const data = await res.json();
+        setPhotoData(data);
+        console.log(data);
+    };
+
+    const fetchDefaultPhoto = async () => {
+        const res = await fetch(
+            `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+        );
+
+        const data = await res.json();
+        setPhotoData(data);
+        console.log(data);
+    };
 
     return (
         <>
-
             <NavBar />
             <div>
                 <div>
-                    <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)} />
+                    <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
+                    <button onClick={() => fetchPhoto(selectedDate)}>Search</button>
                 </div>
 
                 {/* Rest of the content */}
-                <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center" }}>
-                    {photoData.media_type === "image" ? (
-                        <img
-                            src={photoData.url}
-                            alt={photoData.title}
-                            className="photos"
-                            style={{ maxWidth: "30%", height: "auto" }}
-                        />
-                    ) : (
-                        <iframe
-                            title="space-video"
-                            src={photoData.url}
-                            frameBorder="0"
-                            gesture="media"
-                            allow="encrypted-media"
-                            allowFullScreen
-                            className="photos"
-                            style={{ width: "100%", height: "500px" }}
-                        />
-                    )}
-                    <div style={{ margin: "20px" }}>
-                        <h1 style={{ fontSize: "24px", marginBottom: "10px" }}>{photoData.title}</h1>
-                        <p style={{ fontSize: "16px", color: "#666", marginBottom: "10px" }}>{photoData.date}</p>
-                        <p style={{ fontSize: "18px" }}>{photoData.explanation}</p>
+                {photoData && (
+                    <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center" }}>
+                        {photoData.media_type === "image" ? (
+                            <img
+                                src={photoData.url}
+                                alt={photoData.title}
+                                className="photos"
+                                style={{ maxWidth: "30%", height: "auto" }}
+                            />
+                        ) : (
+                            <iframe
+                                title="space-video"
+                                src={photoData.url}
+                                frameBorder="0"
+                                gesture="media"
+                                allow="encrypted-media"
+                                allowFullScreen
+                                className="photos"
+                                style={{ width: "100%", height: "500px" }}
+                            />
+                        )}
+                        <div style={{ margin: "20px" }}>
+                            <h1 style={{ fontSize: "24px", marginBottom: "10px" }}>{photoData.title}</h1>
+                            <p style={{ fontSize: "16px", color: "#666", marginBottom: "10px" }}>{photoData.date}</p>
+                            <p style={{ fontSize: "18px" }}>{photoData.explanation}</p>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </>
     );
-}
+};
 
 export default NasaPhoto;
